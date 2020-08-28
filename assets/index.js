494f137b066c3875faf1ab1ce2d1abaf29cbf01d
Jesus Garcia <jelegaho@gmail.com>
2020-08-28 01:00:45 -0500

    entendiendo como trabajan los services workers para utilizar el cache en modo offline

--- assets/index.js
+++ assets/index.js
@@ -10,3 +10,9 @@ const player = new MediaPlayer( { el: video, plugins:[new AutoPlay(), new AutoPa
 
 button.onclick = () => player.tooglePlay()
 mute.onclick = () => player.toogleMute()
+
+if ('serviceWorker' in navigator){    
+    navigator.serviceWorker.register("/sw.js").catch(error => {
+        console.log(error.message)
+    })
+}
